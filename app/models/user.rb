f4629f8f2b6806e931d2cfc1ad109975f80b52c7
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-09-09 15:00:53 +0900

    [Add]LoginMethod

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,12 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   def fullname
     last_name + first_name
   end
