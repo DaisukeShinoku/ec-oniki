b2ed9135a0b4bf7dcf5444c30a88947e92fd510b
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-09-14 00:20:08 +0900

    [fix]seed

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,8 @@ class HomesController < ApplicationController
   def top
     @categories = Category.where(is_valid: true)
     @areas = Area.all
+    @products = Product.all
+    @gourmet = Product.where(category_id: 1)
   end
   
 end
