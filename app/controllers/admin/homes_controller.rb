e28551964c91aaf72839f94fd80ae40f6f2c2a4e
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-09-13 20:49:20 +0900

    [Fix]AdminLayout

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,6 @@ class Admin::HomesController < ApplicationController
   def top
     @categories = Category.all
     @areas = Area.all
+    @orders = Order.where(created_at: Time.zone.now.all_day)
   end
 end
